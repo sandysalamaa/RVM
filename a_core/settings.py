e232b446b5ed5045349cfd544d6a93049d7035af
Sandy Khalid <Sandy.Khalid@jameelfinance.com.eg>
2025-08-05 00:45:43 +0300

    save jwt cred

--- a_core/settings.py
+++ a_core/settings.py
@@ -15,6 +15,11 @@ import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+    ),
+}
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
